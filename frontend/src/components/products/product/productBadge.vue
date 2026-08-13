d80fe0ee2c4f9dc8da61a620317679886d9dbf1c
246Vansh <246vansh@gmail.com>
2026-08-13 22:49:59 +0530

    fix badges ux

--- frontend/src/components/products/product/productBadge.vue
+++ frontend/src/components/products/product/productBadge.vue
@@ -1,15 +1,31 @@
 <template>
-    <span v-if="badge" :class="badgeClasses">
-        {{ badge.name }}
-    </span>
+
+    <div v-if="badge" class="pointer-events-none absolute left-0 top-0 z-20 h-[92px] w-[92px] overflow-hidden">
+
+        <div :class="badgeClasses">
+
+            <component v-if="badge.icon" :is="badge.icon" class="h-3.5 w-3.5" />
+
+            <span>
+                {{ badge.name }}
+            </span>
+
+        </div>
+
+    </div>
+
 </template>
 
 <script setup>
+
 import { computed } from "vue";
 
 const props = defineProps({
+
     badge: {
+
         type: Object,
+
         default: null,
 
         validator(value) {
@@ -19,32 +35,42 @@ const props = defineProps({
             return (
                 "id" in value &&
                 "name" in value &&
-                "backgroundClass" in value &&
-                "textClass" in value
+                "background" in value &&
+                "text" in value
             );
 
         },
+
     },
+
 });
 
 const badgeClasses = computed(() => [
-
     "absolute",
-    "left-4",
-    "top-4",
-    "z-20",
-    "rounded-full",
-    "px-3",
-    "py-1",
-    "text-xs",
-    "font-semibold",
+    "left-[-27px]",
+    "top-[17px]",
+    "flex",
+    "w-[115px]",
+    "-rotate-45",
+    "items-center",
+    "justify-center",
+    "gap-1",
+    "py-2",
+    "text-[10px]",
+    "font-black",
     "uppercase",
-    "tracking-wide",
-
-    props.badge?.backgroundClass,
-
-    props.badge?.textClass,
-
+    "tracking-[0.1em]",
+    "leading-none",
+    "whitespace-nowrap",
+    "border-y",
+    "border-white/30",
+    "shadow-[0_4px_12px_rgba(15,23,42,0.18)]",
+    "transition-all",
+    "duration-300",
+    "group-hover:translate-x-0.5",
+    "group-hover:shadow-[0_7px_18px_rgba(15,23,42,0.22)]",
+    props.badge?.background,
+    props.badge?.text,
 ]);
 
 </script>
